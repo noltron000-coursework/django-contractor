0ab5c2147ebc459a671090aca91f842e27241269
Nolan Kovacik <noltron000@gmail.com>
2020-03-06 10:53:39 -0800

    [add] better keys index

--- keys/views.py
+++ keys/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Key
 
+
 def index(request):
 	latest_key_list = Key.objects.order_by('-date_added')[:5]
-	output = ', '.join([k.name for k in latest_key_list])
-	return HttpResponse(output)
+	template = loader.get_template('keys/index.html')
+	context = {
+		'latest_key_list': latest_key_list,
+	}
+	return HttpResponse(template.render(context, request))
+
 
 def detail(request, key_id):
 	return HttpResponse(f'You\'re looking at key {key_id}.')
