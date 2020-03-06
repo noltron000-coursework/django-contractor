305f9512bdde145fc93ffd4b8e8a1cabe920d6e2
Nolan Kovacik <noltron000@gmail.com>
2020-03-06 10:47:32 -0800

    [upd] improve keys index

--- keys/views.py
+++ keys/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Key
+
 def index(request):
-	return HttpResponse('Hello, world. You\'re at the keys index.')
+	latest_key_list = Key.objects.order_by('-date_added')[:5]
+	output = ', '.join([k.name for k in latest_key_list])
+	return HttpResponse(output)
 
 def detail(request, key_id):
 	return HttpResponse(f'You\'re looking at key {key_id}.')
