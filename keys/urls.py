0a1616a2f0c1cecdc74aae7bbfae728847ff45dd
Nolan Kovacik <noltron000@gmail.com>
2020-03-06 10:42:04 -0800

    [add] more views

--- keys/urls.py
+++ keys/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# ex: /keys/
 	path('', views.index, name='index'),
+	# ex: /keys/5/
+	path('<int:key_id>/', views.detail, name='detail'),
 ]
--- keys/views.py
+++ keys/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
-
 def index(request):
-	return HttpResponse("Hello, world. You're at the keys index.")
+	return HttpResponse('Hello, world. You\'re at the keys index.')
+
+def detail(request, key_id):
+	return HttpResponse(f'You\'re looking at key {key_id}.')
