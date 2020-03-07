e47242ebb43d2f69da6859705ee05fed87e26479
Nolan Kovacik <noltron000@gmail.com>
2020-03-07 11:30:07 -0800

    [upd] models
    fixing field lengths etc

--- keyboards/models.py
+++ keyboards/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Keyboard(models.Model):
 	def __str__(self):
 		return self.name
-	name = models.CharField(max_length=90)
+	name = models.CharField(max_length=300)
+	description = models.TextField(max_length=90)
 	switch = models.ForeignKey(
 		'switches.Switch',
 		null=True,
 		on_delete=models.SET_NULL
 	)
-	description = models.TextField(max_length=300)
 	date_added = models.DateTimeField('date added')
